8347ae5cc84d9dc917703ab633df313dee4dd2c0
OksanaMinchuk <oks.m89@mail.ru>
2019-04-08 06:54:26 +0300

    update SAX Handler

--- src/by/epam/javatr/minchuk/task06/model/builder/SAX/ITCompanyHandler.java
+++ src/by/epam/javatr/minchuk/task06/model/builder/SAX/ITCompanyHandler.java
@@ -1,9 +1,9 @@
 package by.epam.javatr.minchuk.task06.model.builder.SAX;
 
 import by.epam.javatr.minchuk.task06.model.builder.ITCompanyEnum;
-import by.epam.javatr.minchuk.task06.model.entity.Employee;
-import by.epam.javatr.minchuk.task06.model.entity.ITCompany;
+import by.epam.javatr.minchuk.task06.model.entity.*;
 import org.apache.log4j.Logger;
+import org.xml.sax.Attributes;
 import org.xml.sax.helpers.DefaultHandler;
 
 import java.util.EnumSet;
@@ -25,4 +25,42 @@ public class ITCompanyHandler extends DefaultHandler {
         itCompany = new ITCompany();
         withText = EnumSet.range(ITCompanyEnum.NAME, ITCompanyEnum.PROJECTMANAGER);
     }
+
+    public void startElement(String uri, String localName, String qName, Attributes attrs) {
+
+        ITCompanyEnum temp = ITCompanyEnum.valueOf(localName.toUpperCase());
+
+        switch (temp) {
+            case DEVELOPER:
+                currentEmployee = new Developer();
+                break;
+            case TESTER:
+                currentEmployee = new Tester();
+                break;
+            case PROJECTMANAGER:
+                currentEmployee = new ProjectManager();
+                break;
+            default:
+                if (withText.contains(temp)) {
+                currentEnum = temp;
+            }
+        }
+
+        currentEmployee.setId(attrs.getValue(0));
+
+        if (attrs.getLength() == 2) {
+            currentEmployee.setId(attrs.getValue(1));
+        }
+    }
+
+    public void endElement(String uri, String localName, String qName) {
+
+        ITCompanyEnum temp = ITCompanyEnum.valueOf(localName.toUpperCase());
+
+
+    }
+
+
+
+
 }
